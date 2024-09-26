dc2f2baa67613b5d691c66ed4861d2cf80294759
Youjin Tyan <tyanbz@gmail.com>
2024-09-27 00:23:37 +0500

    update for deploy

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import routes from "./meta";
 
-const MainRouter = createBrowserRouter(routes);
+const MainRouter = createBrowserRouter(routes, {
+    basename: "/yt-blog",
+});
 
 export default MainRouter;
