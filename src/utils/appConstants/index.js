f5a319b0a98fbd5bd159514442b1b3d36f94fb2e
Youjin Tyan <tyanbz@gmail.com>
2024-09-27 16:56:26 +0500

    refactored about page

--- src/utils/appConstants/index.js
+++ src/utils/appConstants/index.js
@@ -1,3 +1,10 @@
 export const storageKeys = {
     theme: "theme",
 }
+
+export const avaImagePath =
+    "https://res.cloudinary.com/duyqvi0ig/image/upload/v1723193765/";
+export const imageKeys = {
+    COLORED: "ava1_o48kxf",
+    GREY: "ava2_xkrf7d",
+};
--- src/utils/constants/index.js
+++ src/utils/constants/index.js
@@ -25,7 +25,7 @@ export const mediaCards = [
         title: "Jasa Kids",
         description:
             "Выступление с темой “IT в нашей жизни” для детей из детских домов Казахстана.",
-        link: "https://www.instagram.com/reel/CvZFO6TszAi",
+        link: "https://t.me/YTvDev/596",
         imagePath:
             "https://res.cloudinary.com/duyqvi0ig/image/upload/v1723146146/1_b10jqw.png",
     },
@@ -61,3 +61,21 @@ export const cta = {
     END_DESCRIPTION: " или нажмите ",
     BUTTON_TEXT: "Написать",
 };
+
+export const descriptionTextAbout =
+    "Мне нравится создавать структуры и функциональные, эстетически привлекательные интерфейсы. Изучаю Web3 и нейросети. В свободное время увлекаюсь музыкой, катаю на борде, играю в настолки и путешествую. Жизнь полна увлекательных открытий!";
+
+export const workingExperience = [
+    "Singularity Net [с окт 2023] / Frontend-разработчик. Разработка интерфейсов для blockchain-сервисов. React.js, Vue.js, Web3",
+    "Choco Family [авг 2022 - янв 2023] / Frontend-разработчик. Работа с интерфейсами магазина ChocoFood. Vue.js, Vite, Vuetify",
+    "xCritical Software [фев 2019 - янв 2020] / Backend-разработчик. Разработка CRM-систем. ASP.NET C#",
+    "Freelance [авг 2018 - мар 2019] / Backend-разработчик. Разработка чат-ботов для Telegram. Python, PostgreSQL, Docker",
+    "Lab of the Future [сен 2014 - авг 2015] / Backend-разработчик. Работа в IBM Domino. Lotus Script",
+    "EduTech [паузы между работой и свбодное время] / 10 лет в сумме обучал программированию и алгоритмизации школьников и студентов. Писал методический материал для школ программирования",
+];
+
+export const skillsDescription = [
+    "JavaScript, TypeScript, React.js, Next.js, Vue3.js",
+    "Python, Telegram API, C#, .NET, ASP.NET, MS SQL Server, PostgreSQL",
+    "Bootstrap, Material-UI, Git, Jira, Trello",
+];
